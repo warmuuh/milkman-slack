f3eb366b8c9b6d04db9595960bfef004a26f46a3
Peter Mucha <peter.mucha@sap.com>
2020-06-20 11:52:41 +0200

    first block api test

--- src/main/java/milkman/slackbot/Application.java
+++ src/main/java/milkman/slackbot/Application.java
@@ -3,13 +3,27 @@ package milkman.slackbot;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.jetty.SlackAppServer;
 
+import static com.slack.api.model.block.Blocks.*;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+import static com.slack.api.model.block.element.BlockElements.asElements;
+import static com.slack.api.model.block.element.BlockElements.button;
+
+
 public class Application {
     public static void main(String[] args) throws Exception {
         // App expects env variables (SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET)
         App app = new App();
 
         app.command("/milkman", (req, ctx) -> {
-            return ctx.ack(":wave: Hello!");
+            return ctx.ack(asBlocks(
+                    section(section -> section.text(markdownText(":wave: pong"))),
+                    actions(actions -> actions
+                            .elements(asElements(
+                                    button(b -> b.actionId("ping-again").text(plainText(pt -> pt.text("Ping"))).value("ping"))
+                            ))
+                    )
+            ));
         });
 
         SlackAppServer server = new SlackAppServer(app, getPort());
