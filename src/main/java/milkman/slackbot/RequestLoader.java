fbbe7e06bcf8ffb01e02485ad48381acd324c63c
Peter Mucha <pmucha@ebay.com>
2020-06-23 15:28:19 +0200

    fixed parsing of request

--- src/main/java/milkman/slackbot/RequestLoader.java
+++ src/main/java/milkman/slackbot/RequestLoader.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import lombok.SneakyThrows;
 import milkman.persistence.UnknownPluginHandler;
 import milkman.plugin.privatebin.PrivateBinApi;
+import milkman.plugin.privatebin.RequestDataContainer;
 import milkman.ui.plugin.rest.domain.RestRequestContainer;
 
 public class RequestLoader {
@@ -16,7 +17,9 @@ public class RequestLoader {
     ObjectMapper mapper = new ObjectMapper();
     mapper.addHandler(new UnknownPluginHandler());
 
-    return mapper.readValue(json, RestRequestContainer.class);
+    RequestDataContainer container = mapper.readValue(json, RequestDataContainer.class);
+
+    return (RestRequestContainer) container.getRequest();
   }
 
 }
