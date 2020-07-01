550f9bafdb5603c06470fdfdf9fb704d5cf62cee
Peter Mucha <peter.mucha@sap.com>
2020-07-01 16:34:23 +0200

    fix installer usage again

--- src/main/java/milkman/slackbot/OauthSlackApp.java
+++ src/main/java/milkman/slackbot/OauthSlackApp.java
@@ -2,8 +2,11 @@ package milkman.slackbot;
 
 import com.slack.api.bolt.App;
 import lombok.Getter;
+import milkman.slackbot.db.BotDatabase;
 import milkman.slackbot.db.Database;
+import milkman.slackbot.db.InstallerDatabase;
 import milkman.slackbot.db.StateDatabase;
+import milkman.slackbot.oauth.JdbcInstallationService;
 import milkman.slackbot.oauth.JdbcOAuthStateService;
 
 @Getter
@@ -13,6 +16,11 @@ public class OauthSlackApp {
 
     public OauthSlackApp(Database db) {
         this.app = new App().asOAuthApp(true);
+
+        var installers = new InstallerDatabase(db);
+        var bots = new BotDatabase(db);
+        app.service(new JdbcInstallationService(app.config(), installers, bots));
+
         var states = new StateDatabase(db);
         app.service(new JdbcOAuthStateService(states));
     }
